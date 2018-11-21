4a01fcf298847d518ac4c620b792b4b474282962
jgagne <justin.gagne@montserrat.edu>
2018-11-20 19:04:38 -0700

    Update script format
    Update comments and indentation.

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-// form validation
+/* form validation */
 
 $("#event-form").validate({
   rules: {
@@ -9,24 +9,24 @@ $("#event-form").validate({
   }
 });
 
-// utm capture
+/* utm capture */
 
 getParameterByName = function(name) {
-query = window.location.search.toString();
-name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-var regexS = "[\\?&]" + name + "=([^&#]*)";
-var regex = new RegExp(regexS);
-results = regex.exec(query);
-if (results == null) return "";
-else return decodeURIComponent(results[1].replace(/\+/g, " "));
+  query = window.location.search.toString();
+  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+  var regexS = "[\\?&]" + name + "=([^&#]*)";
+  var regex = new RegExp(regexS);
+  results = regex.exec(query);
+  if (results == null) return "";
+  else return decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 $(document).ready(function() {
-var $utmSource = $('[name=utm_source]'),
-$utmMedium = $('[name=utm_medium]'),
-$utmContent = $('[name=utm_content]'),
-$utmCampaign = $('[name=utm_campaign]');
-$utmSource.val(getParameterByName('utm_source'));
-$utmMedium.val(getParameterByName('utm_medium'));
-$utmContent.val(getParameterByName('utm_content'));
-$utmCampaign.val(getParameterByName('utm_campaign'));
+  var $utmSource = $('[name=utm_source]'),
+      $utmMedium = $('[name=utm_medium]'),
+      $utmContent = $('[name=utm_content]'),
+      $utmCampaign = $('[name=utm_campaign]');
+  $utmSource.val(getParameterByName('utm_source'));
+  $utmMedium.val(getParameterByName('utm_medium'));
+  $utmContent.val(getParameterByName('utm_content'));
+  $utmCampaign.val(getParameterByName('utm_campaign'));
 });
